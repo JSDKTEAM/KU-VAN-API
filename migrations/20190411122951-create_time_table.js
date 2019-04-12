5e7172c9466e7c500d2a393ee0b5ebeefe18daf8
JSDKTEAM <jetsadakorn1@windowslive.com>
2019-04-12 21:55:32 +0700

    update migrations

--- migrations/20190411122951-create_time_table.js
+++ migrations/20190411122951-create_time_table.js
@@ -16,6 +16,11 @@ module.exports = {
         onUpdate: 'CASCADE',
         onDelete: 'SET NULL',
       },
+      count_seat : {
+        type: Sequelize.INTEGER(2),
+        allowNull: false,
+        defaultValue: 0
+      },
       time_out : {
         type: Sequelize.STRING(10),
         allowNull: false,
