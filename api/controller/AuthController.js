6d1bc8dc3b7ac3a1095cbee7bd2ba6c859859238
JSDKTEAM <jetsadakorn1@windowslive.com>
2019-04-17 17:01:18 +0700

    Update AuthController.js

--- api/controller/AuthController.js
+++ api/controller/AuthController.js
@@ -5,10 +5,10 @@ const jwt = require('jsonwebtoken');
 const db = require('../../src/database/connection');
 const User = db.User;
 
-exports.getUserAccount = async(req,res,next) => {
+exports.getUserAccount = async (req, res, next) => {
     User.findOne({
-        where : {user_id : req.auth.user_id},
-        attributes : ['user_id','username','fname','lname','phoneNumber']
+        where: { user_id: req.auth.user_id },
+        attributes: ['user_id', 'username', 'fname', 'lname', 'phoneNumber']
     }).then(result => {
         res.status(200).json(result);
     })
@@ -20,6 +20,7 @@ exports.login = async (req, res, next) => {
     let user = await User.findOne({
         where: { username: req.body.username },
     });
+
     let { user_id, username, password, type_user } = user.dataValues;
     let checkPwd = passwordHash.verify(req.body.password, password);
     if (checkPwd) {
@@ -29,14 +30,16 @@ exports.login = async (req, res, next) => {
             type_user: type_user
         }, process.env.JWT_KEY
         )
-        res.status(201).json({
+        return res.status(201).json({
             message: "Autn successful",
             token: token
         });
     }
     else {
-        res.status(401).json({
+        return res.status(401).json({
             message: "username or password not corrent"
         });
     }
+
+    next({ message: "username not corrent", status: 401 })
 }
