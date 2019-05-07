8e2983b73892ca6f86f0eacb7d0b7e25d1002be6
JSDKTEAM <jetsadakorn1@windowslive.com>
2019-05-07 22:15:37 +0700

    Update ReserveController.js

--- api/controller/ReserveController.js
+++ api/controller/ReserveController.js
@@ -20,11 +20,22 @@ exports.createReserve = async (req, res, next) => {
                                           INNER JOIN Port ON Port.port_id = Car.port_id
                                           WHERE Time.time_id = ?`, { replacements: [req.body.time_id], type: sequelize.QueryTypes.SELECT });
         if (port.count_seat < port.number_of_seats) {
-                result = await Reserve.create({
-                    time_id: time_id,
-                    user_id: req.auth.user_id,
-                    destination: destination,
-                }, { transaction });
+                if(req.auth.type_user !== "ADMIN"){
+                    result = await Reserve.create({
+                        time_id: time_id,
+                        user_id: req.auth.user_id,
+                        destination: destination,
+                    }, { transaction });
+                }
+                else{
+                    result = await Reserve.create({
+                        time_id: time_id,
+                        user_id: req.auth.user_id,
+                        destination: destination,
+                        phoneNumberWalkIn : phoneNumberWalkIn,
+                        nameWalkIn : nameWalkIn,
+                    }, { transaction });
+                }
 
                 updateTime = await Time.update(
                     { count_seat: ++port.count_seat },
